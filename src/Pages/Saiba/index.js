a48eb1772197fdc3b632eead2fd2e9f28465b67c
Irann Orecchio <129894177+IrannnOrecchio@users.noreply.github.com>
2024-01-28 20:50:47 -0300

    Ultimo commit
    Acrescentei coisas na página de saiba mais.

--- src/Pages/Saiba/index.js
+++ src/Pages/Saiba/index.js
@@ -1,79 +1,78 @@
+import React from 'react';
 import './Saiba.css';
 
-
-
 function Saiba() {
-    return (
-      <div>
-        <h1>SAIBA MAIS – Ouvidoria</h1>
-        <div className='texto-saiba'>
+  return (
+    <div>
+      <h1>SAIBA MAIS – Ouvidoria</h1>
+      <div className='texto-saiba'>
         <p>
           <strong>Representação de Queixas e Denúncias:</strong>
           <ul>
             <li>Relacionadas ao cotidiano da empresa.</li>
             <li>Possibilidade de anexar fotos e vídeos para fundamentar reclamações/denúncias.</li>
           </ul>
-  
+
           <strong>Cadastro na Plataforma:</strong>
           <ul>
             <li>Necessidade de criar um cadastro para acesso.</li>
             <li>Importância de anotar login e senha em local seguro.</li>
           </ul>
-  
+
           <strong>Preservação do Anonimato:</strong>
           <ul>
             <li>Instruções para manter o anonimato durante o cadastro.</li>
             <li>Exemplo de elementos que podem revelar identidade (nome, idade, apelidos).</li>
           </ul>
-  
+
           <strong>Processo de Envio de Demandas:</strong>
           <ul>
             <li>Descrição do procedimento após o cadastro.</li>
             <li>Upload de documentos para esclarecimento das demandas.</li>
           </ul>
-  
+
           <strong>Análise pelo Setor de Ouvidoria:</strong>
           <ul>
             <li>Prazo de 5 dias úteis para visualização do relato.</li>
             <li>Transformação da demanda em processo administrativo.</li>
           </ul>
-  
+
           <strong>Possibilidade de Deleção da Reclamação/Denúncia:</strong>
           <ul>
             <li>Opção de deletar integralmente antes da visualização pelo Setor de Ouvidoria.</li>
           </ul>
-  
+
           <strong>Análise e Andamento do Processo:</strong>
           <ul>
             <li>Mudança de status de "EM ABERTO" para "EM ANDAMENTO" após visualização.</li>
             <li>Compromisso de análise mesmo com desinteresse do reclamante.</li>
           </ul>
-  
+
           <strong>Importância de Detalhes nas Demandas:</strong>
           <ul>
             <li>Solicitação para relatar detalhadamente as demandas.</li>
             <li>Abertura de novo prazo em caso de informações insuficientes.</li>
           </ul>
-  
+
           <strong>Prazo de Resposta e Finalização:</strong>
           <ul>
             <li>Até 90 dias úteis para investigação interna.</li>
             <li>Possibilidade de prorrogação por mais 90 dias.</li>
             <li>Status "FINALIZADA" após apresentação de resposta.</li>
           </ul>
-  
+
           <strong>Arquivamento do Processo:</strong>
           <ul>
             <li>Arquivamento definitivo após 15 dias corridos sem manifestação do reclamante.</li>
             <li>Status "ARQUIVADO".</li>
           </ul>
-  
+
           <strong>Uso Adequado da Ouvidoria:</strong>
           <ul>
             <li>Distinção entre Ouvidoria e ferramentas de pesquisa.</li>
             <li>Recomendação para casos de alta gravidade.</li>
           </ul>
-  
+
           <strong>Compromisso com Imparcialidade e Confidencialidade:</strong>
           <ul>
             <li>Valorização da imparcialidade, transparência e confidencialidade.</li>
@@ -81,8 +80,8 @@ function Saiba() {
           </ul>
         </p>
       </div>
-      </div>
-    );
-  }
-  
-  export default Saiba;
+    </div>
+  );
+}
+
+export default Saiba;
